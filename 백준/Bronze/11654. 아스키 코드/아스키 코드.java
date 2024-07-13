6cf7457658713c0e5bb061230be9ddd2107f64cd
sumina225 <122432989+sumina225@users.noreply.github.com>
2024-07-13 17:54:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		char c = s.charAt(0);
+		System.out.println(c+0);
+	}
+}
