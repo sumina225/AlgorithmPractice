5920c49ec9b0063b8257ce69da0324643457b0ed
sumina225 <122432989+sumina225@users.noreply.github.com>
2025-06-23 18:02:41 +0900

    [level 2] Title: 큰 수 만들기, Time: 9137.72 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,24 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
-        Stack<Character> stack = new Stack<>();
-        for(char c: number.toCharArray()){
-            while(k > 0 && !stack.isEmpty() && stack.peek() < c){
-                stack.pop();
-                k--;
-            }
-            stack.push(c);
-        }
-        while(k > 0){
-            stack.pop();
-            k--;
-        }
-        while(!stack.isEmpty()){
-            answer = stack.peek() + answer;
-            stack.pop();
-        }
         return answer;
     }
 }
