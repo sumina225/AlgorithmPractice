117d53d978ff43916c3a138293a4bc5f19491359
sumina225 <122432989+sumina225@users.noreply.github.com>
2025-08-17 15:30:19 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 228 ms, Memory: 19460 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -4,37 +4,43 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        int[] nums = new int[t];
+
+        int T = Integer.parseInt(br.readLine());
+        int[] nums = new int[T];
         int maxN = 0;
-        for(int i = 0; i < t; i++){
+        for (int i = 0; i < T; i++) {
             nums[i] = Integer.parseInt(br.readLine());
-            if(nums[i] > maxN) maxN = nums[i];
+            if (nums[i] > maxN) maxN = nums[i];
         }
-        boolean[] isPrime = new boolean[maxN+1];
-        Arrays.fill(isPrime,true);
-        isPrime[0] = false;
-        isPrime[1] = false;
-        for(int i = 4; i < maxN; i++){
-            for(int j = 2; j*j <= i; j++){
-                if(i % j == 0){
-                    isPrime[i] = false;
-                    break;
-                }
+
+        // 1) 에라토스테네스 체: O(N log log N)
+        boolean[] isPrime = new boolean[maxN + 1];
+        Arrays.fill(isPrime, true);
+        if (maxN >= 0) isPrime[0] = false;
+        if (maxN >= 1) isPrime[1] = false;
+
+        for (int p = 2; p * p <= maxN; p++) {
+            if (!isPrime[p]) continue;
+            for (int m = p * p; m <= maxN; m += p) {
+                isPrime[m] = false;
             }
         }
-        for(int i = 0; i < t; i++){
-            int num = nums[i];
-            int answer = 0;
-            for(int j = 2; j <= num/2; j++){
-                if(isPrime[j] && isPrime[num-j]){
-                    answer++;
-                }
+
+        // 2) 소수 리스트 미리 생성 → 쿼리 시 소수만 순회
+        ArrayList<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= maxN; i++) if (isPrime[i]) primes.add(i);
+
+        // 3) 각 n에 대해 p ≤ n/2 인 소수만 보고 isPrime[n - p] 체크
+        for (int n : nums) {
+            int cnt = 0;
+            for (int p : primes) {
+                if (p > n / 2) break;
+                if (isPrime[n - p]) cnt++;
             }
-            sb.append(answer).append("\n");
+            sb.append(cnt).append('\n');
         }
-        System.out.println(sb.toString());
+
+        System.out.print(sb.toString());
     }
 }
