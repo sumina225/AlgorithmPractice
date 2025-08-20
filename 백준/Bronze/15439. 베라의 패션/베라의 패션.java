075454239a8e41e72d9c2f8e354b6156d11ac2fd
sumina225 <122432989+sumina225@users.noreply.github.com>
2025-08-20 09:31:54 +0900

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n*(n-1));
+    }
+}
