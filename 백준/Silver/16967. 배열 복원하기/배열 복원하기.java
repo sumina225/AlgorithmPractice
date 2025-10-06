91221f1db692ff43157f750effd702027faecfce
sumina225 <122432989+sumina225@users.noreply.github.com>
2025-10-07 08:38:06 +0900

    [Silver III] Title: 배열 복원하기, Time: 400 ms, Memory: 41896 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+
+        int[][] B = new int[H + X][W + Y];
+        int[][] A = new int[H][W];
+
+        for (int i = 0; i < H + X; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < W + Y; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 복원 시작
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                // 겹치는 부분인 경우, B에서 이미 더해진 값을 빼줘야 함
+                if (i >= X && j >= Y) {
+                    A[i][j] = B[i][j] - A[i - X][j - Y];
+                } else {
+                    A[i][j] = B[i][j];
+                }
+            }
+        }
+
+        // 결과 출력
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                sb.append(A[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
