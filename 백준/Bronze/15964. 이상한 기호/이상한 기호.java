308c25046ffbd726ecb9b20f7e10fe9e10acb4e1
sumina225 <122432989+sumina225@users.noreply.github.com>
2025-11-25 19:59:04 +0900

    [Bronze V] Title: 이상한 기호, Time: 100 ms, Memory: 14244 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        
+        long result = (A + B) * (A - B);  // 그대로 계산
+        
+        System.out.println(result);
+    }
+}
