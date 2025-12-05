8eea7334853fe7883219094737651738bf836b48
sumina225 <122432989+sumina225@users.noreply.github.com>
2025-12-05 15:07:38 +0900

    [Silver II] Title: 민균이의 계략, Time: 128 ms, Memory: 14660 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            max = Math.max(max,dp[i]);
+        }
+        System.out.println(max);
+    }
+}
