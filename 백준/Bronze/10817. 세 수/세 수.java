fcd995039e22d0fc5fa8d008a9f055c1364284ba
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-01-06 15:00:28 +0900

    [Bronze IV] Title: 세 수, Time: 100 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[3];
+        for(int i = 0; i < 3; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        System.out.println(nums[1]);
+    }
+}
