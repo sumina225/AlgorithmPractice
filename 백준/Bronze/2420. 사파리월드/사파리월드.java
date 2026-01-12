895597ea6ef2100820ff49781f54de152eaf9048
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-01-12 13:12:53 +0900

    [Bronze V] Title: 사파리월드, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+        long answer = Math.abs(n-m);
+        System.out.println(answer);
+
+    }
+}
