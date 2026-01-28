16007ad18f3256e9c54e947c840a205b76fe1715
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-01-28 09:08:42 +0900

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
