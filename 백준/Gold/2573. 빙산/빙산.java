0fe46f3a3c5545907960332c2ba23f02cc5a8293
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-03-24 12:39:41 +0900

    [Gold IV] Title: 빙산, Time: 780 ms, Memory: 247976 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -18,69 +18,95 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+
         int[][] nums = new int[n][m];
+        List<Pos> ice = new ArrayList<>();
+
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; j++){
                 nums[i][j] = Integer.parseInt(st.nextToken());
+                if(nums[i][j] > 0){
+                    ice.add(new Pos(i,j));
+                }
             }
         }
-        boolean isZero = false;
+
         int answer = 0;
         int[] dr = {1,-1,0,0};
         int[] dc = {0,0,1,-1};
-        while(!isZero){
-            int count = 0;
+
+        while(true){
+
+            // ✅ 1. 빙산 덩어리 체크 (BFS 1번만)
+            if(ice.isEmpty()){
+                System.out.println(0);
+                return;
+            }
+
             boolean[][] visited = new boolean[n][m];
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(!visited[i][j] && nums[i][j] != 0){
-                        count++;
-                        Queue<Pos> queue = new LinkedList<>();
-                        queue.add(new Pos(i,j));
-                        visited[i][j] = true;
-                        while(!queue.isEmpty()){
-                            Pos pos = queue.poll();
-                            for(int k = 0; k < 4; k++){
-                                int nr = pos.r + dr[k];
-                                int nc = pos.c + dc[k];
-                                if(nr >= 0 && nc >= 0 && nr < n && nc < m && nums[nr][nc] != 0 && !visited[nr][nc]){
-                                    queue.add(new Pos(nr,nc));
-                                    visited[nr][nc] = true;
-                                }
-                            }
-                        }
+            Queue<Pos> queue = new LinkedList<>();
+
+            Pos start = ice.get(0);
+            queue.add(start);
+            visited[start.r][start.c] = true;
+
+            int connected = 1;
+
+            while(!queue.isEmpty()){
+                Pos cur = queue.poll();
+                for(int k = 0; k < 4; k++){
+                    int nr = cur.r + dr[k];
+                    int nc = cur.c + dc[k];
+
+                    if(nr >= 0 && nc >= 0 && nr < n && nc < m
+                        && nums[nr][nc] > 0 && !visited[nr][nc]){
+                        visited[nr][nc] = true;
+                        queue.add(new Pos(nr,nc));
+                        connected++;
                     }
                 }
             }
-            if(count >= 2){
-                break;
-            }else if(count == 0){
-                isZero = true;
+
+            // 👉 덩어리 2개 이상
+            if(connected != ice.size()){
+                System.out.println(answer);
+                return;
             }
+
+            // ✅ 2. 녹이기 (ice만 순회)
+            int[][] melt = new int[n][m];
+
+            for(Pos p : ice){
+                int cnt = 0;
+                for(int k = 0; k < 4; k++){
+                    int nr = p.r + dr[k];
+                    int nc = p.c + dc[k];
+
+                    if(nr >= 0 && nc >= 0 && nr < n && nc < m
+                        && nums[nr][nc] == 0){
+                        cnt++;
+                    }
+                }
+                melt[p.r][p.c] = cnt;
+            }
+
+            // ✅ 3. 실제 반영 + ice 갱신
+            List<Pos> newIce = new ArrayList<>();
+
+            for(Pos p : ice){
+                int r = p.r;
+                int c = p.c;
+
+                nums[r][c] = Math.max(nums[r][c] - melt[r][c], 0);
+
+                if(nums[r][c] > 0){
+                    newIce.add(new Pos(r,c));
+                }
+            }
+
+            ice = newIce;
             answer++;
-            int[][] copy = new int[n][m];
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(nums[i][j] > 0){
-                        int min = 0;
-                        for(int k = 0; k < 4; k++){
-                            int nr = i + dr[k];
-                            int nc = j + dc[k];
-                            if(nr >= 0 && nc >= 0 && nr < n && nc < m && nums[nr][nc] == 0){
-                                min++;
-                            }
-                        }
-                        copy[i][j] = Math.max(nums[i][j] - min, 0);
-                    }else{
-                        copy[i][j] = 0;
-                    }
-                }
-            }
-            for(int i = 0; i < n; i++){
-                nums[i] = copy[i].clone();
-            }
         }
-        System.out.println(isZero ? 0 : answer);
     }
 }
