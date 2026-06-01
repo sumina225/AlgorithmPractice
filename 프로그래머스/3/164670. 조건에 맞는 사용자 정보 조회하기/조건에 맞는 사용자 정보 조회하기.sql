e316a2b41880388e6a17a6653f8963b4b9afb0b2
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-06-01 16:27:50 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,17 @@
+SELECT B.USER_ID,
+       B.NICKNAME,
+       B.CITY || ' ' || B.STREET_ADDRESS1 || ' ' || B.STREET_ADDRESS2 AS 전체주소,
+       SUBSTR(B.TLNO, 1, 3)
+       || '-'
+       || SUBSTR(B.TLNO, 4, 4)
+       || '-'
+       || SUBSTR(B.TLNO, 8, 4) AS 전화번호
+FROM USED_GOODS_USER B
+INNER JOIN (
+    SELECT WRITER_ID
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+    HAVING COUNT(*) >= 3
+) A
+ON A.WRITER_ID = B.USER_ID
+ORDER BY B.USER_ID DESC;
