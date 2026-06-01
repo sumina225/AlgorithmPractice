a6f88944b9285cc1eb08a83f32eb47c925a3eefd
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-06-01 16:40:21 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+SELECT A.BOARD_ID AS BOARD_ID
+     , A.WRITER_ID AS WRITER_ID
+     , A.TITLE AS TITLE
+     , A.PRICE AS PRICE
+     , CASE
+        WHEN A.STATUS = 'SALE'
+        THEN '판매중'
+        WHEN A.STATUS = 'RESERVED'
+        THEN '예약중'
+        ELSE '거래완료'
+       END AS STATUS
+  FROM USED_GOODS_BOARD A
+ WHERE TO_CHAR(A.CREATED_DATE, 'YYYY-MM-DD') = '2022-10-05'
+ ORDER BY A.BOARD_ID DESC
