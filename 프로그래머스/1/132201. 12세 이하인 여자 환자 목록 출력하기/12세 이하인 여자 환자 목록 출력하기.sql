65e4f1dab0bebf32f7b9bf9e33be4b4f68ab843e
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-06-01 14:00:26 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT A.PT_NAME AS PT_NAME
+     , A.PT_NO AS PT_NO
+     , A.GEND_CD AS GEND_CD
+     , A.AGE AS AGE
+     , NVL(A.TLNO, 'NONE') AS TLNO
+  FROM PATIENT A
+ WHERE A.AGE <= 12
+   AND A.GEND_CD = 'W'
+ ORDER BY A.AGE DESC
+        , A.PT_NAME ASC
