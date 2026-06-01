c42e0ba1ceaae2c7665775056b907d18b2303bb1
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-06-01 16:09:21 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT A.ANIMAL_ID AS ANIMAL_ID
+     , A.NAME AS NAME
+     , A.SEX_UPON_INTAKE AS SEX_UPON_INTAKE
+  FROM ANIMAL_INS A
+ WHERE A.NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ ORDER BY A.ANIMAL_ID
