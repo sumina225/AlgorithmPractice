74317e4d72764582f4e0f4fc0848bfe7e830d738
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-06-15 12:49:46 +0900

    [Passed] 비교에 따른 연산 (94ms, 9MB)

---
+++ codetree/trail0/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if(a > b){
+            System.out.println(a*b);
+        } else {
+            System.out.println(b/a);
+        }
+    }
+}
