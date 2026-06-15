11d5898b9004b1160eacc40abe7ce3b95fce6c9a
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-06-15 13:53:16 +0900

    [Passed] 배열의 합 계산하기 (97ms, 9MB)

---
+++ codetree/trail0/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int[] nums = {3, 1, 4, 5, 9};
+        System.out.println(nums[1] + nums[3] + nums[4]);
+    }
+}
