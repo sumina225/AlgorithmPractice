b2602fad97e9be52d5df382b73c8364cd1693fbd
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-08-18 17:05:32 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,10 +1,10 @@
--- 코드를 입력하세요
-SELECT A.ANIMAL_ID AS ANIMAL_ID
-     , A.NAME AS NAME
-     , CASE
-           WHEN SEX_UPON_INTAKE LIKE '%Neutered%'
-             OR SEX_UPON_INTAKE LIKE '%Spayed%'
-           THEN 'O'
-           ELSE 'X'
-       END AS 중성화
+SELECT A.ANIMAL_ID
+     , A.NAME
+     , CASE 
+        WHEN A.SEX_UPON_INTAKE LIKE 'Neutered%'
+          OR A.SEX_UPON_INTAKE LIKE 'Spayed%'
+        THEN 'O'
+        ELSE 'X'
+       END
   FROM ANIMAL_INS A
+ ORDER BY A.ANIMAL_ID
