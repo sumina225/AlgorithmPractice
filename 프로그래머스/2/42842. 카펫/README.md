81285fb8a53d941a76b9390c74d35b7434c14b29
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-09-04 13:20:58 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 0.05 ms
+메모리: 74.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 21일 18:02:37
+2026년 09월 04일 13:20:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        int sum = brown + yellow;
-        for(int i = 1; i <= sum; i++){
-            int j = 0;
-            if(sum % i == 0){
-                j = sum / i;
-            }else{
+        int total = brown + yellow;
+
+        for (int h = 3; h <= total; h++) {
+            if (total % h != 0) {
                 continue;
             }
-            if(brown == 2*i + 2*j - 4){
-                answer = new int[]{j,i};
-                break;
+
+            int w = total / h;
+
+            if (w < h) {
+                continue;
             }
-            
+
+            if ((w - 2) * (h - 2) == yellow) {
+                return new int[] {w, h};
+            }
         }
-        return answer;
+
+        return new int[] {};
     }
 }
