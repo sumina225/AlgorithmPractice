59b587b777d28e8972f824ff5168882910399585
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-09-09 11:30:25 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            answer += n % 2;
+            n /= 2;
+        }
+
+        return answer;
+    }
+}
