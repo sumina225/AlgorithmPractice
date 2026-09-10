5da08c25e8a0aa249762f8232d7a11d19e2e1a28
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-09-10 11:05:20 +0900

    [level 2] Title: 롤케이크 자르기, Time: 114.80 ms, Memory: 205 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -4,22 +4,17 @@ class Solution {
     public int solution(int[] topping) {
         int answer = 0;
         Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < topping.length; i++){
-            map.merge(topping[i],1,Integer::sum);
-        }
         Set<Integer> set = new HashSet<>();
         for(int i = 0; i < topping.length; i++){
+            map.put(topping[i], map.getOrDefault(topping[i],0) + 1);
+        }
+        for(int i = 0; i < topping.length; i++){
+            map.put(topping[i], map.get(topping[i]) - 1);
             set.add(topping[i]);
-            int cnt = map.get(topping[i]) - 1;
-            if(cnt <= 0){
-                if(map.containsKey(topping[i])){
-                    map.remove(topping[i]);   
-                }
-            }else{
-                map.put(topping[i],cnt);
+            if(map.get(topping[i]) == 0){
+                map.remove(topping[i]);
             }
-            if(set.size() > map.size()) break;
-            if(set.size() == map.size()){
+            if(map.size() == set.size()) {
                 answer++;
             }
         }
