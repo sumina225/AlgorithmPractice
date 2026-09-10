94891e37dc30cbae005f985045224a79d33fb7f7
sumina225 <122432989+sumina225@users.noreply.github.com>
2026-09-10 15:50:49 +0900

    [level 2] Title: 방문 길이, Time: 0.08 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
-메모리: 85.5 MB, 시간: 0.45 ms
+메모리: 82.2 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 09일 15:39:53
+2026년 09월 10일 15:50:46
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,47 +1,57 @@
-import java.util.*;
-
 class Solution {
-    static class Pos{
-        int x;
-        int y;
-    }
-    
     public int solution(String dirs) {
         int answer = 0;
         boolean[][][] visited = new boolean[11][11][4];
-        Pos pos = new Pos();
-        pos.x = 5;
-        pos.y = 5;
+        // 상하좌우순으로
+        // start는 5,5
+        int r = 5;
+        int c = 5;
         for(int i = 0; i < dirs.length(); i++){
-            char c = dirs.charAt(i);
-            if(c == 'U' && pos.y < 10){
-                visited[pos.x][pos.y][0] = true;
-                pos.y++;
-                visited[pos.x][pos.y][1] = true;
-
-            }else if(c == 'D' && pos.y > 0){
-                visited[pos.x][pos.y][1] = true;
-                pos.y--;
-                visited[pos.x][pos.y][0] = true;
-            }else if(c == 'R' && pos.x < 10){
-                visited[pos.x][pos.y][2] = true;
-                pos.x++;
-                visited[pos.x][pos.y][3] = true;
-            }else if(c == 'L' && pos.x > 0){
-                visited[pos.x][pos.y][3] = true;
-                pos.x--;
-                visited[pos.x][pos.y][2] = true;
+            char cc = dirs.charAt(i);
+            int nr = r;
+            int nc = c;
+            if(cc == 'U'){
+                nr++;
+            }else if(cc == 'D'){
+                nr--;
+            }else if(cc == 'L'){
+                nc--;
+            }else if(cc == 'R'){
+                nc++;
             }
-        }
-        for(int i = 0; i < 11; i++){
-            for(int j = 0; j < 11; j++){
-                for(int k = 0; k < 4; k++){
-                    if(visited[i][j][k]){
+            if(nr < 0 || nr > 10 || nc < 0 || nc > 10){
+                nr = r;
+                nc = c;
+            }else{
+                if(cc == 'U'){
+                    if(!visited[r][c][0]){
+                        visited[r][c][0] = true;
+                        visited[nr][nc][1] = true;
+                        answer++;
+                    }
+                }else if(cc == 'D'){
+                    if(!visited[r][c][1]){
+                        visited[r][c][1] = true;
+                        visited[nr][nc][0] = true;
+                        answer++;
+                    }
+                }else if(cc == 'L'){
+                    if(!visited[r][c][2]){
+                        visited[r][c][2] = true;
+                        visited[nr][nc][3] = true;
+                        answer++;
+                    }
+                }else if(cc == 'R'){
+                    if(!visited[r][c][3]){
+                        visited[r][c][3] = true;
+                        visited[nr][nc][2] = true;
                         answer++;
                     }
                 }
+                r = nr;
+                c = nc;
             }
         }
-        return answer/2;
+        return answer;
     }
 }
